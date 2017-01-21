4492b43bcedce1b7c6cecb7a9e602f5fe19eaca7
Kelsey <kpnewman2013@gmail.com>
2017-01-21 18:46:38 -0500

    logic for comparing guess from FE to correct answer

--- src/main/java/com/theironyard/controllers/MasterMindController.java
+++ src/main/java/com/theironyard/controllers/MasterMindController.java
@@ -3,9 +3,13 @@ package com.theironyard.controllers;
 import com.theironyard.entities.MasterMind;
 import com.theironyard.services.MasterMindRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 /**
  * Created by kelseynewman on 1/20/17.
@@ -34,10 +38,45 @@ public class MasterMindController {
             games.save(masterMind);
         }
     }
+    //while round <= 12, check guess against correct answer
+    //if round > 12 end game and return correct answer
+
+    @RequestMapping(path = "/guess", method = RequestMethod.POST)
+    //we take in their guess and compare it to randomly generated guess in spot one of our guess table
+    //store that guess in our table
+    //check against our correct answer through our checkGuess method
+    //store the checks array that is generated through that method in the checks column of our table
+
+    @RequestMapping(path = "/answer", method = RequestMethod.GET)
+    //return to FE the checks array that was generated in post method
 
 
     public static int randomNumber() {
         return (int)((Math.random()* 8) + 1);
+    }
+
+    public int [] checkGuess(int [] guess) {
+        int red = 2;
+        int white = 1;
+        int noMatch = 0;
+        int [] checks = new int [4];
+        int [] correctAnswer = games.findById(1).guesses;
+        for (int i = 0; i < guess.length; i ++) {
+            for (int j = 0; j < correctAnswer.length; j++) {
+                if (guess[i] == correctAnswer[j]) {
+                    checks[i] = red;
+                    //TODO find a way to take j out of the array that is being compared so that repeated numbers in guess aren't checked against same number in correctAnswer
+                    break;
+                } else if (Arrays.asList(correctAnswer).contains(guess[i])) {
+                    checks[i] = white;
+                    break;
+                } else {
+                    checks[i] = noMatch;
+                    break;
+                }
+            }
+        }
+        return checks;
     }
 
 }
